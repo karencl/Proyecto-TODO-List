dde0773fc88c5923222b058726a207a14b80ced9
karencl <a01704254@itesm.mx>
2020-10-13 10:57:49 -0500

    Modificación de estructura y comentarios

--- FuncionesTodoList.py
+++ FuncionesTodoList.py
@@ -1,12 +1,16 @@
-"""Biblioteca extra usada"""
-import os   # Ésta biblioteca me permite consultar si un archivo existe o no.
-            # Y la utilizaré para verificar si el usuario que está corriendo el programa,
-            # ya tiene su archivo o es necesario crear uno nuevo.
+# ----------------- Biblioteca extra usada -----------------
+import os
+"""
+    Ésta biblioteca me permite consultar si un archivo existe o no.
+    Y la utilizaré para verificar si el usuario que está corriendo el programa,
+    ya tiene su archivo o es necesario crear uno nuevo.
+"""
+
+# ----------------- Funciones utilizadas en el programa -----------------
 
 
-"""Funciones utilizadas en el programa"""
-
-def solicitaDatosPerfil():
+def solicita_datos_perfil():
+    """Solicita los datos del usuario, para su perfil"""
     matricula = input("Ingresa tu matrícula: ")
     correo = input("Ingres tu correo electrónico: ")
     num_semestre = int(input("Cuéntanos cuál semestre te encuentras cursando: "))
@@ -14,7 +18,8 @@ def solicitaDatosPerfil():
     return matricula, correo, num_semestre, edad
 
 
-def mostrarPerfil(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes):
+def mostrar_perfil(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes):
+    """Muestra el perfil del usuario"""
     print("PERFIL de", nombre)
     print("··················································")
     print("Nombre:", nombre)
@@ -30,8 +35,8 @@ def mostrarPerfil(nombre, matricula, correo, edad, num_semestre, num_materias, n
     print()
 
 
-# Imprime la lista de materias
-def mostrarMaterias(lista_materias):
+def mostrar_materias(lista_materias):
+    """Imprime la lista de materias"""
     print("Tus materias:")
     j = 1
     for i in lista_materias:
@@ -40,41 +45,45 @@ def mostrarMaterias(lista_materias):
     return lista_materias
 
 
-# Imprime la lista de pendientes (dentro de la lista general de pendientes), de la materia deseada
-def mostrarPendientes(lista_materias, lista_pendientes, materia_seleccionada):
+def mostrar_pendientes(lista_materias, lista_pendientes, materia_seleccionada):
+    """Imprime la lista de pendientes (dentro de la lista general de pendientes), de la materia deseada"""
     print("Pendientes de:", lista_materias[materia_seleccionada - 1])
     k = 1
     for i in lista_pendientes[materia_seleccionada - 1]:
         print(k, "-", i)
         k += 1
-    tiempoPendientes(k)
+    tiempo_pendientes(k)
 
 
-# Calcula el tiempo estimado para completar los pendientes de una materia
-def tiempoPendientes(n):
+def tiempo_pendientes(n):
+    """Calcula el tiempo estimado para completar los pendientes de una materia"""
     print("** Tiempo estimado para completar pendientes:", (n - 1) * 45, "min **")
     return n
 
 
-# Agrega un pendiente a la lista de pendientes (dentro de la lista general de pendientes), de la materia deseada
-def agregarPendiente(lista, materia_seleccionada: int):
+def agregar_pendiente(lista, materia_seleccionada: int):
+    """Agrega un pendiente a la lista de pendientes (dentro de la lista general de pendientes), de la materia deseada"""
     nuevo_pendiente = input("Ingresa pendiente: ")
     lista[materia_seleccionada - 1].append(nuevo_pendiente)
 
 
-# Elimina un pendiente de la lista de pendientes (dentro de la lista general de pendientes), de la materia deseada
-def tacharPendiente(lista, materia_seleccionada: int):
-    tachar_pendiente = int(input("¿Cuál es el pendiente que quieres tachar? "))
-    lista[materia_seleccionada - 1].pop(tachar_pendiente - 1)
+def tachar_pendiente(lista, materia_seleccionada: int):
+    """Elimina un pendiente de la lista de pendientes (dentro de la lista general de pendientes),
+    de la materia deseada"""
+    pendiente_a_tachar = int(input("¿Cuál es el pendiente que quieres tachar? "))
+    lista[materia_seleccionada - 1].pop(pendiente_a_tachar - 1)
 
 
-"""--------------Nuevas funciones para el archivo del usuario--------------------"""
+# --------------Nuevas funciones para el archivo del usuario--------------------
 
-def existeArchivo(ruta):
+
+def existe_archivo(ruta):
+    """Revisa si el archivo con la ruta (nombre) dada, existe"""
     return os.path.isfile(ruta)
 
 
-def leerUsuario(nombre):
+def leer_usuario(nombre):
+    """Lee el archivo del usuario para obtener sus datos"""
     lista_pendientes = []
     archivo_usuario = open(nombre + ".usuario", "r")
     nombre = archivo_usuario.readline().rstrip()
@@ -94,10 +103,13 @@ def leerUsuario(nombre):
         pen_temp = pendientes.split()
         lista_pendientes.append(pen_temp)
     archivo_usuario.close()
-    return nombre, matricula, correo, edad, num_semestre, num_materias, lista_materias, num_total_pendientes, lista_pendientes
+    return nombre, matricula, correo, edad, num_semestre, num_materias, \
+        lista_materias, num_total_pendientes, lista_pendientes
 
 
-def escribirNuevoUsuario(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes, lista_materias, lista_pendientes):
+def escribir_nuevo_usuario(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes,
+                           lista_materias, lista_pendientes):
+    """Si no existe ningún archivo para el usuario, crea uno nuevo"""
     archivo_usuario = open(nombre + ".usuario", "w")
     archivo_usuario.write(nombre + "\n")
     archivo_usuario.write(matricula + "\n")
@@ -110,8 +122,7 @@ def escribirNuevoUsuario(nombre, matricula, correo, edad, num_semestre, num_mate
     archivo_usuario.write(str(lista_materias) + "\n")
     archivo_usuario.write(str(num_total_pendientes) + "\n")
 
-    # Guarda cada lista dentro de la lista pendientes, en el archivo .usuario (como Str)
+    # Guarda cada lista dentro de la lista pendientes, en el archivo .usuario (como Str). Cada una en una línea distinta
     for i in lista_pendientes:
         archivo_usuario.write(str(i) + "\n")
     archivo_usuario.close()
-
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import FuncionesTodoList as Funciones
 
 # Listas utilizadas:
-LISTA_MATERIAS = []   # Lista "simple"
-LISTA_PENDIENTES = [] # Lista "simple", que posteriormente se le agregan más listas, convirtiéndola en una lista anidada
+LISTA_MATERIAS = []    # Lista "simple"
+LISTA_PENDIENTES = []  # Lista "simple", que posteriormente se le agregan más listas, convirtiéndola en lista anidada
 
-print("Bienvenid@! \n¿Cómo te llamas?")
+print("Bienvenid@! \n¿Cómo te llamas?: ")
 
 # Solicita nombre del usuario
 nombre = input()
-print("Hola", nombre, "\nEsto es TODO List. \n"
-                      "Aquí podrás anotar todos los pendientes que tengas,"
-                      "con el fin de llevar una mejor organización"
-                      "en tu día a día.")
+print("Hola", nombre,   "\nEsto es TODO List. \n"
+                        "Aquí podrás anotar todos los pendientes que tengas,"
+                        "con el fin de llevar una mejor organización"
+                        "en tu día a día.")
 print()
 
 # Revisa si el archivo con el nombre existe
-if Funciones.existeArchivo(nombre + ".usuario"):
+if Funciones.existe_archivo(nombre + ".usuario"):
     print("Leyendo datos del usuario...")
     print()
-    (nombre, matricula, correo, edad, num_semestre, num_materias, LISTA_MATERIAS, num_total_pendientes, LISTA_PENDIENTES) = Funciones.leerUsuario(nombre)
+    (nombre, matricula, correo, edad, num_semestre, num_materias, LISTA_MATERIAS,
+     num_total_pendientes, LISTA_PENDIENTES) = Funciones.leer_usuario(nombre)
 else:
     # Si no, se solicitan los datos del usuario
     print("Danos tus datos para poder empezar!")
-    matricula, correo, num_semestre, edad = Funciones.solicitaDatosPerfil()
+    matricula, correo, num_semestre, edad = Funciones.solicita_datos_perfil()
     num_materias = 0
     num_total_pendientes = 0
     print()
@@ -30,7 +31,7 @@ else:
 # Imprime perfil del usuario
 print("Creando perfil...")
 print()
-Funciones.mostrarPerfil(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes)
+Funciones.mostrar_perfil(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes)
 print("Perfil creado con éxito! \nAhora estás list@ para empezar!")
 print()
 
@@ -53,18 +54,18 @@ while opcion_seleccionada != 7:
     if opcion_seleccionada == 1:
         print("¿Cómo se llama la materia que quieres ingresar?")
         nombre_nueva_materia = input("Nombre: ")
-        LISTA_MATERIAS.append(nombre_nueva_materia) # Se añade la materia a la lista de materias
+        LISTA_MATERIAS.append(nombre_nueva_materia)  # Se añade la materia a la lista de materias
         LISTA_PENDIENTES.append([])  # Se crea una nueva lista en la lista de pendientes, para la materia creada
         num_materias += 1
         print()
 
     # opción 2 seleccionada (borrar materia)
     elif opcion_seleccionada == 2:
-        Funciones.mostrarMaterias(LISTA_MATERIAS)
+        Funciones.mostrar_materias(LISTA_MATERIAS)
         print()
         materia_seleccionada_op2 = int(input("¿Qué materia quieres borrar? "))
         print("Se eliminará la materia:", LISTA_MATERIAS[materia_seleccionada_op2 - 1])
-        LISTA_MATERIAS.remove(LISTA_MATERIAS[materia_seleccionada_op2 - 1]) # Se elimina la materia de la lista materias
+        LISTA_MATERIAS.remove(LISTA_MATERIAS[materia_seleccionada_op2 - 1])  # Se elimina la materia de LISTA_MATERIAS
         num_materias -= 1
         num_total_pendientes -= len(LISTA_PENDIENTES[materia_seleccionada_op2 - 1])
         LISTA_PENDIENTES.remove(LISTA_PENDIENTES[materia_seleccionada_op2 - 1])  # Se elimina la lista de pendientes de
@@ -73,23 +74,23 @@ while opcion_seleccionada != 7:
 
     # opción 3 seleccionada (agregar pendiente)
     elif opcion_seleccionada == 3:
-        Funciones.mostrarMaterias(LISTA_MATERIAS)
+        Funciones.mostrar_materias(LISTA_MATERIAS)
         materia_seleccionada_op3 = int(input("¿A qué materia quieres ingresar un nuevo pendiente? "))
         print()
         print("Nuevo pendiente para la materia:", LISTA_MATERIAS[materia_seleccionada_op3 - 1])
-        Funciones.agregarPendiente(LISTA_PENDIENTES, materia_seleccionada_op3)
+        Funciones.agregar_pendiente(LISTA_PENDIENTES, materia_seleccionada_op3)
         num_total_pendientes += 1
         print()
 
     # opción 4 seleccionada (ver pendientes por materia)
     elif opcion_seleccionada == 4:
-        Funciones.mostrarMaterias(LISTA_MATERIAS)
+        Funciones.mostrar_materias(LISTA_MATERIAS)
         materia_seleccionada_op4 = int(input("¿De qué materia quieres ver tus pendientes? "))
         print()
-        Funciones.mostrarPendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
+        Funciones.mostrar_pendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
         print()
-        salir = False
-        while salir == False:
+        opcion4 = True
+        while opcion4:
             print("¿Qué quieres hacer?")
             print("1) Agregar nuevo pendiente")
             print("2) Tachar un pendiente")
@@ -100,27 +101,27 @@ while opcion_seleccionada != 7:
 
             # opción 1 de opción 4 (agregar nuevo pendiente)
             if opcion == 1:
-                Funciones.agregarPendiente(LISTA_PENDIENTES, materia_seleccionada_op4)
+                Funciones.agregar_pendiente(LISTA_PENDIENTES, materia_seleccionada_op4)
                 num_total_pendientes += 1
                 print()
-                Funciones.mostrarPendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
+                Funciones.mostrar_pendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
                 print()
 
             # opción 2 de opción 4 (tachar pendiente)
             elif opcion == 2:
-                Funciones.mostrarPendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
+                Funciones.mostrar_pendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
                 print()
-                Funciones.tacharPendiente(LISTA_PENDIENTES, materia_seleccionada_op4)
+                Funciones.tachar_pendiente(LISTA_PENDIENTES, materia_seleccionada_op4)
                 num_total_pendientes -= 1
                 print()
-                Funciones.mostrarPendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
+                Funciones.mostrar_pendientes(LISTA_MATERIAS, LISTA_PENDIENTES, materia_seleccionada_op4)
                 print()
 
             # opción 3 de opción 4 (volver al menú principal)
             elif opcion == 3:
                 print("Volviendo al menú principal...")
                 print()
-                salir = True
+                opcion4 = False
 
             # opción erronea de opción 4 (mensaje de error)
             else:
@@ -129,19 +130,20 @@ while opcion_seleccionada != 7:
 
     # opción 5 seleccionada (ver perfil)
     elif opcion_seleccionada == 5:
-        Funciones.mostrarPerfil(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes)
+        Funciones.mostrar_perfil(nombre, matricula, correo, edad, num_semestre, num_materias, num_total_pendientes)
         print()
 
     # opción 6 seleccionada (editar perfil)
     elif opcion_seleccionada == 6:
-        matricula, correo, num_semestre, edad = Funciones.solicitaDatosPerfil()
+        matricula, correo, num_semestre, edad = Funciones.solicita_datos_perfil()
         print()
 
     # opción 7 seleccionada (salir)
     elif opcion_seleccionada == 7:
         print("Has decidido salir.")
         print("Guardando datos del usuario...")
-        Funciones.escribirNuevoUsuario(nombre, matricula, correo, edad, num_semestre, num_materias, LISTA_MATERIAS, num_total_pendientes, LISTA_PENDIENTES)
+        Funciones.escribir_nuevo_usuario(nombre, matricula, correo, edad, num_semestre, num_materias, LISTA_MATERIAS,
+                                         num_total_pendientes, LISTA_PENDIENTES)
         print()
 
     # opción erronea (mensaje de error)
